8adf76405bb9f78b484c1059099074fd758040a4
Banu Qedirova <qedirovabanu0@gmail.com>
2025-09-25 20:51:10 +0400

    fixed

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,11 +1,24 @@
 import s from "./style.module.scss";
+import { GrClose } from "react-icons/gr";
 
 export default function ToDoList({ todos }) {
   return (
     <div className={s.wrapper}>
-      {todos.map((todo) => (
-        <div key={todo.id}>{todo.title}</div>
-      ))}
+      {todos.length > 0 ? (
+        todos.map((todo) => (
+          <div key={todo.id} className={s.card}>
+            <div className={s.content}>
+              <h1>{todo.title}</h1>
+              <p>{todo.about}</p>
+            </div>
+            <button className={s.btn}>
+              <GrClose size={15} color="#FF8303" />
+            </button>
+          </div>
+        ))
+      ) : (
+        <h1 className={s.noTasks}>No tasks</h1>
+      )}
     </div>
   );
 }
--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -4,24 +4,22 @@ import ToDoForm from "../../components/ToDoForm/ToDoForm";
 import ToDoList from "../../components/ToDoList/ToDoList";
 
 export default function List() {
-  const [todos, setTodos] = useState([
-    { id: 1, title: "hello world", about: "sfsf" },
-    { id: 2, title: "hello world2", about: "sfsf" },
-    { id: 3, title: "hello world3", about: "sfsf" },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const [todoTitle, setTodoTitle] = useState("");
   const [todoAbout, setTodoAbout] = useState("");
 
   return (
     <div className="container">
-      <ToDoForm
-        todoTitle={todoTitle}
-        setTodoTitle={setTodoTitle}
-        todoAbout={todoAbout}
-        setTodoAbout={setTodoAbout}
-      />
-      <ToDoList todos={todos} />
+      <div className={s.wrapper}>
+        <ToDoForm
+          todoTitle={todoTitle}
+          setTodoTitle={setTodoTitle}
+          todoAbout={todoAbout}
+          setTodoAbout={setTodoAbout}
+        />
+        <ToDoList todos={todos} />
+      </div>
     </div>
   );
 }
